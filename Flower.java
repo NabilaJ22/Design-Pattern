e9c787bb81852dae6cc10c0c94a3f78127b6427b
NabilaJ22 <121808041+NabilaJ22@users.noreply.github.com>
2023-07-14 22:30:29 +0600

    Task 2 Flower.java

--- Flower.java
+++ Flower.java
@@ -45,6 +45,46 @@ public void bloom()
         System.out.println("The sunflower follows the sun's movement.");
     }
 }
+//Method with parameters
+public class Rose  {
+    private String color;
+
+    public Rose(String color) {
+        this.color = color;
+    }
+
+    public void bloom() {
+        System.out.println("The " + color + " flower blooms.");
+    }
+
+}
+public class Sunflower  {
+    private String color;
+
+    public Sunflower(String color) {
+        this.color = color;
+    }
+
+    public void bloom() {
+        System.out.println("The " + color + " flower blooms.");
+    }
+
+}
+
+// Main
+    public class  Main 
+{
+    public static void main(String[] args) {
+        Rose redRose = new Rose("Red");
+        redRose.bloom();
+        redRose.prick();
+
+        System.out.println();
+        Sunflower yellowSunflower = new Sunflower("Yellow");
+        yellowSunflower.bloom();
+        yellowSunflower.followSun();
+    }
+}
 
 
 
