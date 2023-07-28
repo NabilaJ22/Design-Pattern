f31c0807ee8738a3d4d418bb39f7c4f5450862c5
Nabila <121808041+NabilaJ22@users.noreply.github.com>
2023-07-28 21:45:34 +0600

    Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -57,3 +57,20 @@ interface Vehicle {
         }
     }
 }
+
+//main function
+public class Main {
+    public static void main(String[] args) {
+
+        Vehicle.Car car = new Vehicle.Car();
+        car.setPrice(15.00);
+        car.start();
+        car.stop();
+
+        Vehicle.Aeroplane aeroplane = new Vehicle.Aeroplane();
+        aeroplane.setPrice(1000);
+        aeroplane.start();
+        aeroplane.fly();
+        aeroplane.stop();
+    }
+}
